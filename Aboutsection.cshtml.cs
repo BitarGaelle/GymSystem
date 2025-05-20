15df601c3305c88af2896a84c34c70c9071f614f
Gaelle Bitar <bitargaelle7@gmail.com>
2025-05-20 19:01:26 +0300

    Add project files.

---
+++ Aboutsection.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymSystem
+{
+    public class AboutsectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
