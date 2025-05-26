392cf77545d78ee48687c926d81d4b5889b529c3
Gaelle Bitar <bitargaelle7@gmail.com>
2025-05-26 21:04:42 +0300

    Backend: Dashboard, Client, Activity, Membership

--- Pages/AdminPage/ClientPage/ClientPage.cshtml.cs
+++ Pages/AdminPage/ClientPage/ClientPage.cshtml.cs
@@ -6,14 +6,18 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
+using GymSystem.Models;
+using GymSystem.Services;
 
 public class ClientPageModel : PageModel
 {
     private readonly HttpClient _httpClient;
+    private readonly IGymService _gymService;
 
-    public ClientPageModel(IHttpClientFactory clientFactory)
+    public ClientPageModel(IHttpClientFactory clientFactory, IGymService gymService)
     {
         _httpClient = clientFactory.CreateClient();
+        _gymService = gymService;
     }
 
     public List<Client> Clients { get; set; } = new();
@@ -28,17 +32,13 @@ public class ClientPageModel : PageModel
     {
         try
         {
-            var response = await _httpClient.GetAsync("http://localhost:3003/client");
-            response.EnsureSuccessStatusCode();
-
-            var json = await response.Content.ReadAsStringAsync();
-            Clients = JsonSerializer.Deserialize<List<Client>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            Clients = await _gymService.GetAllClientsAsync();
 
             if (!string.IsNullOrWhiteSpace(SearchQuery))
             {
                 var query = SearchQuery.ToLower();
                 FilteredClients = Clients
-                    .Where(c => $"{c.FirstName} {c.LastName}".ToLower().Contains(query))
+                    .Where(c => $"{c.client_fname} {c.client_lname}".ToLower().Contains(query))
                     .ToList();
             }
             else
@@ -53,24 +53,4 @@ public class ClientPageModel : PageModel
     }
 
 
-}
-
-//issue i encountered, the [JsonPropertyName] wasn't the same as the database column name
-//so fname and lname weren't showing
-public class Client
-{
-    [JsonPropertyName("client_fname")]
-    public string FirstName { get; set; }
-
-    [JsonPropertyName("client_lname")]
-    public string LastName { get; set; }
-
-    [JsonPropertyName("address")]
-    public string Address { get; set; }
-
-    [JsonPropertyName("phone")]
-    public string Phone { get; set; }
-
-    [JsonPropertyName("email")]
-    public string Email { get; set; }
 }
